3329f126b701c95ed200f238064e9465ad6f9395
Vitthal Gadekar <gadekarvitthal2@gmail.com>
2025-07-06 16:45:24 +0530

    env

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../envitoments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:5000/api/auth';
+  // private baseUrl = 'http://localhost:5000/api/auth';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
